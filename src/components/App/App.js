3c7667387334d87071b42d82e470ffc371a5d956
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-01-14 21:38:04 +0000

    Create bolierplate files architecture: remove unused files, add reset.css, font links and updated favicon and static components.

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Ja<span class="highlight">mmm</span>ing
+      </h1>
+      <div className="App">
+        {/* SearchBar */}
+        <div className="App-playlist">
+          {/* SearchResults */}
+          {/* PlayList */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
