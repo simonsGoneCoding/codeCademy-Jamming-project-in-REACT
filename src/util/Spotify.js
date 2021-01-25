9bca0bf6fc7c265554a7ef8479764731658891db
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-01-25 17:39:26 +0000

    Obtain spotify access token.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,6 @@
 let accessToken;
+const clientID = "74d42106c0504306adb1268b50362ec8";
+const redirectUri = "http://localhost:3000/";
 
 const Spotify = {
   getAccessToken() {
@@ -13,6 +15,12 @@ const Spotify = {
     if (accessTokenMatch && expiresInMatch) {
       accessToken = accessTokenMatch[1];
       const expiresIn = Number(expiresInMatch[1]);
+      //clears the parameters, allowing to grab a new access token when it expires
+      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/");
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = accessUrl;
     }
   },
 };
